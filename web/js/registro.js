22363b4003c6f8ebcd00ed1f181c17a607ac88d2
Oscar <oscarse150802@gmail.com>
2024-02-16 21:05:35 +0100

    Registro con barra navegadora y fondo

--- web/js/registro.js
+++ web/js/registro.js
@@ -11,3 +11,18 @@ function validarCorreo(){
 function validarDni(){
 
 }
+/**
+ * Con esta funcion hacemos que desaparezca la barra navegadora
+ */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").classList.remove("hidden"); // Muestra la barra de navegación al hacer scroll hacia arriba
+  } else {
+    document.getElementById("navbar").classList.add("hidden"); // Oculta la barra de navegación al hacer scroll hacia abajo
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+
